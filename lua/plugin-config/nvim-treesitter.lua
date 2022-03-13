c6917ea16b25a43f6eb0096bbb6f9db3c843bd80
everpcpc <git@everpcpc.com>
2022-03-13 18:50:13 +0800

    chore: disable treesitter lua

--- lua/plugin-config/nvim-treesitter.lua
+++ lua/plugin-config/nvim-treesitter.lua
@@ -13,7 +13,7 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
 
     -- list of language that will be disabled
-    disable = { },
+    disable = { 'lua' },
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
